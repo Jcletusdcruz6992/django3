10eef7c9d9ecd9f8dc2864f26d9c7b84b91ec70c
James Cletus Dcruz <jamescletusdcruz@gmail.com>
2022-04-03 15:46:26 -0400

    .

--- ProTwo/ProTwo/urls.py
+++ ProTwo/ProTwo/urls.py
@@ -1,4 +1,4 @@
-"""ProTwo URL Configuration
+"""ProTwo URL Configuratio
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
